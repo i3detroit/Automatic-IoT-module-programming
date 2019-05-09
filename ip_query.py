39b7c96cfc693cbf3ab1f20f48b5ec97e91d8b71
Mark Furland <mtfurlan@i3detroit.org>
2019-05-09 00:18:55 -0400

    ip_query works

--- ip_query.py
+++ ip_query.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from espq import *
+import espq
 
 RED = '\033[1;31m'
 GREEN = '\033[1;32m'
 NOCOLOR = '\033[0m'
 
-d=import_devices()
+d=espq.import_devices('tasmota.json')
 for device in d:
-    device.query_status5()
-    if '10.13.107' in device.reported_ip:
+    device.query_tas_status(espq.network_attr)
+    if device.reported_ip == device.ip_addr:
         print(('{GREEN}{name}{NOCOLOR}\t{reported_ip}\t'
                '{reported_mac}'.format(GREEN=GREEN,
                                        NOCOLOR=NOCOLOR,
