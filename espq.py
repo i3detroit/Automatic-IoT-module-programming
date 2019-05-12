c12f24bd7f764064a2dec61859496bb8a3ea26a5
mkfink <mkfink@gmail.com>
2019-05-11 23:17:48 -0400

    fix hass switch config

--- espq.py
+++ espq.py
@@ -306,9 +306,9 @@ class device(dict):
         elif self.domain == 'switch':
             import hass_templates.switch
             with open(hass_output_dir + '/switch_{name}.yaml'.format(**self), 'w') as yamlf:
-                yamlf.write(hass_templates.light.light.format(**self))
+                yamlf.write(hass_templates.switch.switch.format(**self))
             with open(hass_output_dir + '/sensors_{name}.yaml'.format(**self), 'w') as yamlf:
-                yamlf.write(hass_templates.light.sensors.format(**self))
+                yamlf.write(hass_templates.switch.sensors.format(**self))
 
 def import_devices(device_file, flash_mode = 'wifi', serial_port = ''):
     """
