d947633e8a35299d34f7188805863832b6ebbf24
mkfink <mkfink@gmail.com>
2019-05-28 11:35:12 -0400

    added terminal title change per device

--- flash.py
+++ flash.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 import argparse
 import espq
+import sys
 
 parser = argparse.ArgumentParser(description='Compile and flash ESPs')
 
-parser.add_argument('-m', '--mode', dest='flashMode',
-                    action='store', choices=('wifi', 'serial'),
+parser.add_argument('-m', '--mode', 
+                    dest='flashMode',
+                    action='store',
+                    choices=('wifi', 'serial'),
                     default='serial',
                     help='set flash mode')
-parser.add_argument('--no-online-check', dest='onlineCheck', action='store_false',
-                     help='Don\'t check if it comes online')
-parser.add_argument('--pause', dest='pauseBeforeFlash', action='store_true',
-                     help='Ask for input before flashing')
-parser.add_argument('-p', '--port', dest='serialPort', action='store',
-                     default='/dev/ttyUSB0', help='Specify serial port to flash from')
-parser.add_argument('-v', '--verbose', dest='verbosity', action='count',
-                     default=0, help='Add verbosity')
+parser.add_argument('--no-online-check',
+                    dest='onlineCheck',
+                    action='store_false',
+                    help='Don\'t check if it comes online')
+parser.add_argument('--pause',
+                    dest='pauseBeforeFlash',
+                    action='store_true',
+                    help='Ask for input before flashing')
+parser.add_argument('-p', '--port',
+                    dest='serialPort',
+                    action='store',
+                    default='/dev/ttyUSB0',
+                    help='Specify serial port to flash from')
+parser.add_argument('-v', '--verbose',
+                    dest='verbosity',
+                    action='count',
+                    default=0,
+                    help='Add verbosity')
 parser.add_argument('deviceFile', metavar='deviceFile',
                      help='What file to load device defs from')
 
@@ -25,5 +38,8 @@ input("Press Enter to continue...")
 
 devices = espq.import_devices(args.deviceFile)
 
-for dev in devices:
+for count, dev in enumerate(devices, start=1):
+    sys.stdout.write(('\x1b]2Processing device {count}/{total} - {name}'
+                      '\x07').format(count=count, total=len(devices),
+                                     name=dev.name))
     dev.flash(args.flashMode, args.serialPort)
