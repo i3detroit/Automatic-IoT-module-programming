f97911611b866333476afe6bb9326152b0ec2985
mkfink <mkfink@gmail.com>
2019-10-18 13:40:49 -0400

    adds interactive UI for picking devices

--- flash.py
+++ flash.py
@@ -4,6 +4,7 @@ import espq
 import sys
 import os
 import re
+from PyInquirer import style_from_dict, Token, prompt, Separator
 
 def fmtcols(mylist, cols):
     """
@@ -18,16 +19,27 @@ def fmtcols(mylist, cols):
 
 
 def progress_report(devices):
-    print("\nProgress report:")
+    """ Print a list of devices and their flashing status """
     formatted_status = fmtcols(["[{status}] {d}".format(status = 'X' if dev.flashed == True else ' ', d=dev.name) for dev in devices], 3)
     print(re.sub('\[X\]', '[\033[1;32mX\033[0m]', formatted_status))
     print('=' * cols)
 
+# Style for selection interface
+style = style_from_dict({
+    Token.Separator: '#FF00AA',
+    Token.QuestionMark: '#00AAFF bold',
+    Token.Selected: '#00AAFF',  # default
+    Token.Pointer: '#00FF00 bold',
+    Token.Instruction: '#FFAA00',  # default
+    Token.Answer: '#00AAFF bold',
+    Token.Question: '#FF00AA',
+})
 
 colors = {"RED": '\033[1;31m',
           "GREEN": '\033[1;32m',
           "NOCOLOR": '\033[0m'}
 
+# get terminal window size for pretty printing
 try:
     cols, rows = os.get_terminal_size()
 except:
@@ -35,7 +47,6 @@ except:
     rows = 40
 
 parser = argparse.ArgumentParser(description='Compile and flash ESPs')
-
 parser.add_argument('-m', '--mode',
                     dest='flashMode',
                     action='store',
@@ -62,32 +73,50 @@ parser.add_argument('-v', '--verbose',
                     help='Add verbosity')
 parser.add_argument('deviceFile', metavar='deviceFile',
                     help='What file to load device defs from')
-
 args = parser.parse_args()
-print(args)
-input('Press Enter to continue...')
 
 devices = espq.import_devices(args.deviceFile)
 
-for count, dev in enumerate(devices, start=1):
-    progress_report(devices)
+# Ask the user to choose which devices to flash
+questions = [
+    {
+        'type': 'checkbox',
+        'message': 'Select Devices',
+        'name': 'device_selection',
+        'choices': [{'name': '{f_name}'.format(**device)} for device in devices],
+        'validate': lambda answer: 'You must choose at least one device.' if len(answer) == 0 else True
+    }
+]
+answers = prompt(questions, style=style)
+selected_devices = [device for device in devices if device['f_name'] in answers['device_selection']]
+
+print(args)
+print('Devices to be flashed')
+progress_report(selected_devices)
+input('Press Enter to continue...')
+
+# Do the flashing
+for count, dev in enumerate(selected_devices, start=1):
+    print("\nProgress report:")
+    progress_report(selected_devices)
     # set terminal title to show progress
     sys.stdout.write(('\x1b]2Processing device {count}/{total} - {name}'
-                      '\x07').format(count=count, total=len(devices),
+                      '\x07').format(count=count, total=len(selected_devices),
                                      name=dev.name))
     
     if args.pauseBeforeFlash == True:
         print('Ready to process device {count}/{total} - {name}'.format(count=count,
-                                                                  total=len(devices),
+                                                                  total=len(selected_devices),
                                                                   name=dev.name))
         input('Press Enter to continue...')
     else:
         print('Processing device {count}/{total} - {name}'.format(count=count,
-                                                                  total=len(devices),
+                                                                  total=len(selected_devices),
                                                                   name=dev.name))
     dev.flash(args.flashMode, args.serialPort)
     # reset terminal title to something useful like hostname
     sys.stdout.write('\x1b]2{hostname}\x07'.format(hostname=os.uname()[1]))
 
-progress_report(devices)
+print("\nResult report:")
+progress_report(selected_devices)
 print("Done.")
