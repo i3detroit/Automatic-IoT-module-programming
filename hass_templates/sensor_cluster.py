a1cb7590d96cddce69188a6a40000ffe0c795a07
mkfink <mkfink@gmail.com>
2019-10-25 14:03:35 -0400

    added more hass config templates

---
+++ hass_templates/sensor_cluster.py
@@ -0,0 +1,58 @@
+# Simple tasmota light and metadata
+# Use for sonoff basic, touch, SV
+
+import generic
+
+sensor_cluster = """- platform: mqtt
+  name: '{f_name} Temperature'
+  state_topic: 'tele/{base_topic}/{topic}/SENSOR'
+  unit_of_measurement: '°C'
+  value_template: '{{{{ value_json['BME280']['Temperature'] }}}}'
+  force_update: True
+  availability_topic: 'tele/{base_topic}/{topic}/LWT'
+  payload_available: 'Online'
+  payload_not_available: 'Offline'
+
+- platform: mqtt
+  name: '{f_name} Pressure'
+  state_topic: 'tele/{base_topic}/{topic}/SENSOR'
+  unit_of_measurement: 'hPa'
+  value_template: '{{{{ value_json['BME280']['Pressure'] }}}}'
+  force_update: True
+  availability_topic: 'tele/{base_topic}/{topic}/LWT'
+  payload_available: 'Online'
+  payload_not_available: 'Offline'
+
+- platform: mqtt
+  name: '{f_name} Humidity'
+  state_topic: 'tele/{base_topic}/{topic}/SENSOR'
+  unit_of_measurement: '%'
+  value_template: '{{{{ value_json['BME280']['Humidity'] }}}}'
+  force_update: True
+  availability_topic: 'tele/{base_topic}/{topic}/LWT'
+  payload_available: 'Online'
+  payload_not_available: 'Offline'
+
+- platform: mqtt
+  name: '{f_name} Lux'
+  state_topic: 'tele/{base_topic}/{topic}/SENSOR'
+  unit_of_measurement: 'lux'
+  value_template: '{{{{ value_json['BH1750']['Illuminance'] }}}}'
+  force_update: True
+  availability_topic: 'tele/{base_topic}/{topic}/LWT'
+  payload_available: 'Online'
+  payload_not_available: 'Offline'
+
+- platform: template
+  sensors:
+    {name}_pressure_kpa:
+      friendly_name: "{f_name} Pressure"
+      unit_of_measurement: 'kPa'
+      value_template: "{{{{ states('sensor.{name}_pressure') | multiply(1/10) | round(1) }}}}"
+"""
+
+sensor = generic.generic_telemetry + sensor_cluster
+
+components = {
+                'sensor': sensor
+             }
