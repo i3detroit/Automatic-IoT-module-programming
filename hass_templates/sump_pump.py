9c91082a40547f9f59134a5a9de7e9abf80dc63f
abzman <abzman2000@gmail.com>
2020-04-01 04:52:48 +0100

    added sump pump template

---
+++ hass_templates/sump_pump.py
@@ -0,0 +1,21 @@
+# Sump Pump measurement, just distance sensor
+
+import generic
+
+sensor_cluster = """- platform: mqtt
+  name: '{f_name} Distance'
+  state_topic: 'tele/{base_topic}/{topic}/SENSOR'
+  unit_of_measurement: 'cm'
+  value_template: "{{{{ value_json['SR04']['Distance'] }}}}"
+  force_update: True
+  availability_topic: 'tele/{base_topic}/{topic}/LWT'
+  payload_available: 'Online'
+  payload_not_available: 'Offline'
+
+"""
+
+sensor = generic.sensor + generic.generic_telemetry + sensor_cluster
+
+components = {
+                'sensor': sensor
+             }
