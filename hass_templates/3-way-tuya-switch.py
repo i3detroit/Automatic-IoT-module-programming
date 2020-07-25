bf8bbe1b4ecef9995bfc79d17e76600d05366898
mkfink <mkfink@gmail.com>
2020-07-25 16:46:17 -0400

    add hass template for 3-way switch

---
+++ hass_templates/3-way-tuya-switch.py
@@ -0,0 +1,31 @@
+# 3-way tasmota light switch and metadata
+# Use for tuya-based 3-way switch like:
+#  - https://templates.blakadder.com/moes-SS01-1.html
+#  - https://templates.blakadder.com/treatlife_SS01S.html
+# If both 3-way switches in a pair are running tasmota,
+# only one needs to be loaded in Home Assistant,
+# though the second one can be as well to track its metadata
+
+import generic
+
+light = """- platform: mqtt
+  name: '{f_name}'
+  state_topic: 'stat/{base_topic}/{topic}/POWER1'
+  command_topic: 'cmnd/{base_topic}/{topic}/EVENT'
+  json_attributes_topic: 'tele/{base_topic}/{topic}/STATE'
+  availability_topic: 'tele/{base_topic}/{topic}/LWT'
+  payload_available: 'Online'
+  payload_not_available: 'Offline'
+  payload_on: 'ON'
+  payload_off: 'OFF'
+"""
+
+sensor = generic.generic_telemetry + """    {name}_power:
+      friendly_name: '{f_name} Power'
+      value_template: '{{{{ states.{hass_domain}.{name}.attributes.POWER }}}}'
+"""
+
+components = {
+                'light': light,
+                'sensor': sensor
+             }
